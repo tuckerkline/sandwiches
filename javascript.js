37bb244a07e47f15cce07c55d69f830fe903c2d6
Tucker Kline <tucker.kline@gmail.com>
2015-09-30 12:24:39 -0600

    comments adding works

--- javascript.js
+++ javascript.js
@@ -1,31 +1,40 @@
 angular.module('sandwichApp', [])
 
 var sandwichControllerFunc = function($scope) {
+
 	$scope.sandwiches = []
 	$scope.submit = function() {
 		var newSandwich = {}
 		newSandwich.name = $scope.name
 		newSandwich.recipe = $scope.recipe
+		newSandwich.ingredients = $scope.ingredients
+		newSandwich.comments = []
 		$scope.sandwiches.push(newSandwich)
 		$scope.name = undefined
 		$scope.recipe = undefined
+		$scope.ingredients = []
 	}
 	$scope.delete = function(index) {
 		$scope.sandwiches.splice(index, 1)
 	}
 
+	$scope.ingredients = []
+	$scope.add = function() {
+        $scope.ingredients.push($scope.ingredientName);
+        $scope.ingredientName = '';
+    };
+
+
+    $scope.addComment = function(sandwichIndex) {
+    	$scope.sandwiches[sandwichIndex].comments.push($scope.sandwiches[sandwichIndex].addedComment)
+    	$scope.sandwiches[sandwichIndex].addedComment = undefined
+   	}
+
+ 
+    
 
 
 
 }
-
-
-
-
-
-
-
-
-
 
 angular.module('sandwichApp').controller('sandwichController', ['$scope', sandwichControllerFunc])
