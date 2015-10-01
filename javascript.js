69f9cc43696dd66ec3ce36bf7d2c39f4b9938672
Tucker Kline <tucker.kline@gmail.com>
2015-10-01 10:56:05 -0600

    ingredient selector works

--- javascript.js
+++ javascript.js
@@ -36,6 +36,33 @@ var sandwichControllerFunc = function($scope) {
     	$scope.sandwiches[sandwichIndex].addedComment = undefined
    	}
 
+
+   	$scope.showByIngredient = false;
+   	$scope.ingredientSelect = function (ingredientIndex, sandwichIndex) {
+   		// console.log(ingredientIndex)
+   		// console.log(sandwichIndex)
+   		$scope.showByIngredient = true;
+   		$scope.selectedIngredientSandwiches = []
+   		var ingredient = $scope.sandwiches[sandwichIndex].ingredients[ingredientIndex]
+   		
+
+   		for (var i = 0; i < $scope.sandwiches.length; i++) {
+   			for ( var j = 0; j < $scope.sandwiches[i].ingredients.length; j++)
+		   		if ($scope.sandwiches[i].ingredients[j].toString().toLowerCase() === ingredient.toString().toLowerCase()) {
+		   			$scope.selectedIngredientSandwiches.push($scope.sandwiches[i])
+		   		}
+   		}
+
+   		console.log(ingredient)
+   		console.log($scope.selectedIngredientSandwiches)
+   		$scope.selectedIngredientName = ingredient
+
+   	}
+   	$scope.close = function() {
+   	$scope.showByIngredient = false;
+
+   	}
+
  
     
 
